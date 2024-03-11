f20b6e8c174db514b066352103601ee08424db1e
Heather Gonzago <hgonzago@hotmail.com>
2024-03-10 23:22:05 -0400

    wip

---
+++ data-from-anywhere/src/helpers/renderFunctions.js
@@ -0,0 +1,320 @@
+import LabelClass from "@arcgis/core/layers/support/LabelClass";
+import PopupTemplate from "@arcgis/core/PopupTemplate";
+import * as typeRendererCreator from "@arcgis/core/smartMapping/renderers/type";
+
+// update the ogcLayer's renderer
+export async function updateLayer(layer, view) {
+  const renderer = await createRenderer(layer, view);
+  layer.renderer = renderer;
+  const template = await createPopupTemplate(layer);
+  layer.popupTemplate = template;
+}
+
+// create the unique types renderer from the cities point layer
+async function createRenderer(layer, view) {
+  const { renderer } = await typeRendererCreator.createRenderer({
+    layer: layer,
+    view: view,
+    field: "STATE",
+    basemapTheme: "light",
+    sortBy: "count",
+    sizeOptimizationEnabled: true,
+  });
+  return renderer;
+}
+
+// create a clustered feature reduction from the points
+export async function createFeatureReductionCluster() {
+  const featureReduction = {
+    type: "cluster",
+    clusterRadius: "100px",
+    clusterMinSize: "19px",
+    clusterMaxSize: "60px",
+    popupEnabled: true,
+    popupTemplate: await createClusterPopupTemplate(),
+    labelingInfo: await createLabelingInfo(),
+  };
+  return featureReduction;
+}
+
+// create labelingInfo to pass into the cluster
+
+async function createLabelingInfo() {
+  var labelInfos = [];
+
+  const labelClass = new LabelClass({
+    deconflictionStrategy: "none",
+    labelExpressionInfo: {
+      expression: `$feature["cluster_count"]
+      var value = $feature["cluster_count"];
+      var num = Count(Text(Round(value)));
+      var label = When(num < 4, Text(value, "#.#"),
+      num == 4, Text(value / Pow(10, 3), "#.0k"),
+      num <= 6, Text(value / Pow(10, 3), "#k"),
+      num == 7, Text(value / Pow(10, 6), "#.0m"),
+      num > 7, Text(value / Pow(10, 6), "#m"),
+      Text(value, "#,###")  );
+      return label;`,
+    },
+    labelPlacement: "center-center",
+    repeatLabel: true,
+    symbol: {
+      type: "text",
+      color: [240, 240, 240, 255],
+      font: {
+        family: "Noto Sans",
+        size: 13,
+        weight: "bold",
+      },
+      horizontalAlignment: "center",
+      kerning: true,
+      haloColor: [55, 56, 55, 255],
+      haloSize: 1.3,
+      rotated: false,
+      verticalAlignment: "baseline",
+    },
+  });
+  labelInfos.push(labelClass);
+
+  return labelInfos;
+}
+
+
+// Create cluster popup template
+
+async function createClusterPopupTemplate() {
+  const clusterPopupTemplate = new PopupTemplate({
+    content: [
+      {
+        type: "text",
+        text: "<p>This cluster represents <b>{cluster_count}</b> cities that fall within the eclipse's totality path. The following lists the amount of clustered cities within each state. </p>",
+      },
+      {
+        type: "expression",
+        expressionInfo: {
+        expression: `Expects($aggregatedFeatures, "STATE")
+    var statsFS = GroupBy($aggregatedFeatures,
+      [{ 
+        name: 'StateName',
+        expression: 'STATE'
+      }], 
+      // Statistics to return for each state and how many cities are in each state cluster
+      [{
+        name: 'num_features',
+        expression: '1',
+        statistic: 'COUNT' 
+      }]
+    );
+    
+    var ordered = OrderBy(statsFs, 'num_features DESC');
+    var list = "<ul>";
+    
+    for (var group in ordered){
+      list += \`<li>\${group.StateName} (\${Text(group.num_features, "#,###")})</li>\`
+    }
+    list += "</ul>";
+    
+    return {
+      type: "text",
+      text: list
+    }`,
+    title: "States with city count"
+      }
+    }
+    ],
+    description:
+      "This cluster represents <b>{cluster_count}</b> cities within the eclipse's totality path.",
+    expressionInfos: [
+      {
+        // ExpressionInfo to label individual state clusters with count
+        name: "cluster_type_STATE",
+        title: "Predominant State",
+        expression: `var uvInfos = [{
+          "value": "OH",
+          "label": "OH"
+        },{
+          "value": "TX",
+          "label": "TX"
+        },{
+          "value": "IN",
+          "label": "IN"
+        },{
+          "value": "NY",
+          "label": "NY"
+        },{
+          "value": "AR",
+          "label": "AR"
+        },{
+          "value": "IL",
+          "label": "IL"
+        },{
+          "value": "MO",
+          "label": "MO"
+        },{
+          "value": "VT",
+          "label": "VT"
+        },{
+          "value": "PA",
+          "label": "PA"
+        },{
+          "value": "OK",
+          "label": "OK"
+        },{
+          "value": "KY",
+          "label": "KY"
+        },{
+          "value": "ME",
+          "label": "ME"
+        },{
+          "value": "NH",
+          "label": "NH"
+        }];
+        
+        var predominantType = Text($feature["cluster_type_STATE"]);
+        var label = "None";
+        
+        for (var i = 0; i < Count(uvInfos); i++) {
+          if (uvInfos[i].value == predominantType) {
+            label = uvInfos[i].label;
+            break;
+          }
+        }
+        return label;`,
+        returnType: "string",
+      },
+    ],
+    fieldInfos: [
+      {
+        fieldName: "cluster_count",
+        format: {
+          digitSeparator: true,
+          places: 0,
+        },
+        label: "Number of features",
+        visible: true,
+      },
+      {
+        fieldName: "expression/cluster_type_STATE",
+        visible: false,
+      },
+    ],
+    title: "Cluster summary",
+  });
+  return clusterPopupTemplate;
+}
+
+async function createPopupTemplate(layer){
+  const popupTemplate = new PopupTemplate({
+    outFields: ["*"],
+    content: [
+      {
+        type: "text",
+        text: "<p>The city of <i>{NAME} </i>will begin 100% totality at</b/> {expression/expr0}</b> and will last for <b>{expression/expr1} </b>.</p>",
+      },
+    ],
+    description:
+      "<p>The city of <i>{NAME} </i>will begin 100% totality at<b> {expression/expr0}</b> and will last for <b>{expression/expr1} </b>.</p>",
+    expressionInfos: [
+      {
+        name: "expr0",
+        title: "Get totality time",
+        expression: `var inputDate = $feature.UTC_Time_100__totality;
+        var hours = Hour(inputDate);
+        var minutes = Minute(inputDate);
+        var seconds = Second(inputDate);
+
+        var amPm = "AM";
+        
+        if (hours >= 12) {
+          amPm = "PM";
+          
+          if (hours > 12) {
+            hours = hours - 12
+          }
+        }
+        var formattedTime = Text(hours, '00') + ':' + Text(minutes, '00') + ':' + Text(seconds, '00') + ' ' + amPm;
+        
+        return formattedTime;`,
+        returnType: "string"
+      },
+      {
+        name: "expr1",
+        title: "durationInMinutes",
+        expression: `var durationInSeconds = $feature.Duration;
+          var minutes = (durationInSeconds / 60);
+          var seconds = durationInSeconds % 60;
+          var formattedMinutes = Text(minutes, '00') + " minutes ";
+          var formattedSeconds = Text(seconds, '00') + " minutes";
+          var combineFormat = formattedMinutes + formattedSeconds
+          
+          return combineFormat;`,
+      },
+    ],
+    fieldInfos: [
+      {
+        fieldName: "OBJECTID",
+        label: "OBJECTID",
+        visible: false,
+      },
+      {
+        fieldName: "NAME",
+        label: "NAME",
+        visible: true,
+      },
+      {
+        fieldName: "Duration",
+        format: {
+          digitSeparator: true,
+          places: 0,
+        },
+        label: "Duration",
+        visible: false,
+      },
+      {
+        fieldName: "STATE",
+        label: "State",
+        visible: true,
+      },
+      {
+        fieldName: "UTC_Time_0_01____end",
+        label: "UTC Time 0.01% - end",
+        visible: false,
+      },
+      {
+        fieldName: "UTC_Time_0_01____start",
+        label: "UTC Time 0.01% - start",
+        visible: false,
+      },
+      {
+        fieldName: "UTC_Time_100__totality",
+        format: {
+          dateFormat: "short-date-short-time",
+          digitSeparator: false,
+        },
+        label: "UTC Time 100% totality",
+        visible: true,
+      },
+      {
+        fieldName: "UTC_Time_50____end",
+        label: "UTC Time 50% - end",
+        visible: true,
+      },
+      {
+        fieldName: "UTC_Time_50____start",
+        label: "UTC Time 50% - start",
+        visible: true,
+      },
+      {
+        fieldName: "expression/expr0",
+        visible: false,
+      },
+      {
+        fieldName: "expression/expr1",
+        visible: false,
+      },
+    ],
+    title: "City: {NAME}",
+  });
+
+      return popupTemplate;
+}
---
+++ data-from-anywhere/src/main.js
@@ -0,0 +1,222 @@
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/WebMap";
+import WFSLayer from "@arcgis/core/layers/WFSLayer";
+import OGCFeatureLayer from "@arcgis/core/layers/OGCFeatureLayer";
+import Legend from "@arcgis/core/widgets/Legend";
+import FeatureEffect from "@arcgis/core/layers/support/FeatureEffect";
+import FeatureFilter from "@arcgis/core/layers/support/FeatureFilter";
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils.js";
+import * as renderUtilities from "./helpers/renderFunctions.js";
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
+
+import "./style.css";
+
+// Load the Calcite custom elements
+defineCustomElements(window, {
+  resourcesUrl: "https://js.arcgis.com/calcite-components/2.6.0/assets",
+});
+
+// Call function to define UI
+setUp();
+
+const map = new WebMap({
+  portalItem: {
+    id: "1ae947834c464227b09ef330bf10eb94",
+  },
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map,
+  popup: {
+    defaultPopupTemplateEnabled: true, // default popup will be enabled on layers
+  },
+});
+
+// Instantiate the OGCFeatureLayer and set initial properties
+// Create OGCFeatureLayer
+const ogcLayer = new OGCFeatureLayer({
+  url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Cities_within_path/OGCFeatureServer", // url to the OGC landing page
+  collectionId: "0", // unique id of the collection (OverlayedFeatures)
+  legendEnabled: true,
+});
+
+const wfsLayer = new WFSLayer({
+  // portalItem: {
+  //   id: "c2a1ec86c6874b7595bbe0bbc26d8e5f",
+  // }
+  url: "https://dservices.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/services/US_states_wfs/WFSServer", // url to your WFS endpoint
+  name: "US_states_polygons", // name of the FeatureType
+  copyright: "Esri"
+});
+
+// once the view is ready, get the layer view from the ogc layer and update
+view.when(async () => {
+  map.when(() => {
+    let activeWidget;
+
+    const handleActionBarClick = ({ target }) => {
+      if (target.tagName !== "CALCITE-ACTION") {
+        return;
+      }
+
+      if (activeWidget) {
+        document.querySelector(`[data-action-id=${activeWidget}]`).active =
+          false;
+        document.querySelector(`[data-panel-id=${activeWidget}]`).hidden = true;
+      }
+
+      const nextWidget = target.dataset.actionId;
+      if (nextWidget !== activeWidget) {
+        document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+        document.querySelector(`[data-panel-id=${nextWidget}]`).hidden = false;
+        activeWidget = nextWidget;
+      } else {
+        activeWidget = null;
+      }
+    };
+
+    document
+      .querySelector("calcite-action-bar")
+      .addEventListener("click", handleActionBarClick);
+
+    let actionBarExpanded = false;
+
+    document.addEventListener("calciteActionBarToggle", (event) => {
+      actionBarExpanded = !actionBarExpanded;
+      view.padding = {
+        left: actionBarExpanded ? 157 : 45,
+      };
+    });
+
+    document.querySelector("calcite-loader").hidden = true;
+  });
+
+  // Create the legend and set the view to it
+  const legend = new Legend({
+    container: "legend-panel",
+    view,
+  });
+});
+
+// Set up the user interface
+
+function setUp() {
+  const toggleModalEl = document.getElementById("toggle-modal");
+  const navigationEl = document.getElementById("nav");
+  const panelEl = document.getElementById("sheet-panel");
+  const modalEl = document.getElementById("modal");
+  const sheetEl = document.getElementById("sheet");
+  const addWfs = document.getElementById("add-wfs-layer");
+  const effectWfs = document.getElementById("add-wfs-effect");
+  const addOgc = document.getElementById("add-ogc-layer");
+  const renderOgc = document.getElementById("add-ogc-renderer");
+  const clusterOgc = document.getElementById("ogc-cluster");
+
+  toggleModalEl?.addEventListener("click", () => handleModalChange());
+  navigationEl?.addEventListener("calciteNavigationActionSelect", () =>
+    handleSheetOpen()
+  );
+
+  panelEl?.addEventListener("calcitePanelClose", () => handlePanelClose());
+
+  addWfs?.addEventListener("click", () => handleAddWfsLayer());
+  effectWfs?.addEventListener("click", () => handleEffectWFS(wfsLayer));
+  
+  addOgc?.addEventListener("click", () => handleAddOgcLayer());
+  renderOgc?.addEventListener("click", () => handleRendererOgc(ogcLayer));
+  clusterOgc?.addEventListener("click", () => handleOgcCluster());
+
+
+  //----------------------
+  // BEGIN DEMO STEPS
+  //----------------------
+
+  // Step 1 - Add the WFS Layer to the map and render
+
+  async function handleAddWfsLayer() {
+    const statesRenderer = {
+      type: "simple",
+      symbol: {
+        type: "simple-fill",
+        color: [0, 0, 0, 0],
+        outline: {
+          color: [191,180,158, 1],
+          width: "1",
+        },
+        style: "solid",
+      },
+    };
+
+    wfsLayer.renderer = statesRenderer;
+    map.add(wfsLayer, 0);
+  }
+
+  // Step 2 - Add feature effect to WFS layer
+  async function handleEffectWFS(wfsLayer) {
+    const featureFilter = new FeatureFilter({
+      where:
+        "(state_name = 'Kentucky') OR (state_name = 'Arkansas') OR (state_name = 'Pennsylvania') OR (state_name = 'Illinois') OR (state_name = 'Ohio') OR (state_name = 'Indiana') OR (state_name = 'New York') OR (state_name = 'Oklahoma') OR (state_name = 'Texas') OR (state_name = 'Missouri') OR (state_name = 'Vermont') OR (state_name = 'New Hampshire') OR (state_name = 'Maine')",
+    });
+
+    const includedEffect =
+      "drop-shadow(1.5px, 1.5px, 2.25px, black) brightness(65%)";
+    const excludedEffect = "opacity(65%) grayscale(100%)";
+
+    const featureEffect = new FeatureEffect({
+      filter: featureFilter,
+      excludedEffect: excludedEffect,
+      includedEffect: includedEffect,
+    });
+
+    const wfsLayerView = await view.whenLayerView(wfsLayer);
+    wfsLayerView.featureEffect = featureEffect;
+  }
+
+  // Step 3 - add OGC Layer to the map
+  function handleAddOgcLayer() {
+    map.add(ogcLayer);
+  }
+
+  //Step 4 - Render the OGC layer by unique value and cluster
+  async function handleRendererOgc(layer) {
+    // once the view is ready, get the layer view from the ogc layer and update
+    const ogcLayerView = await view.whenLayerView(layer);
+    await reactiveUtils.whenOnce(
+      () => !ogcLayerView.updating,
+      renderUtilities.updateLayer(layer, view)
+    );
+  }
+
+  // Step 5 - Set a feature reduction cluster on OGC layer
+
+  async function handleOgcCluster() {
+
+    const featureReduction = await renderUtilities.createFeatureReductionCluster();
+    ogcLayer.featureReduction = featureReduction;
+
+  }
+
+  // ----------------------
+  // END DEMO STEPS
+  // ----------------------
+
+
+  function handleModalChange() {
+    if (modalEl) {
+      modalEl.open = !modalEl.open;
+    }
+  }
+
+  function handleSheetOpen() {
+    sheetEl.open = true;
+    panelEl.closed = false;
+  }
+
+  function handlePanelClose() {
+    sheetEl.open = false;
+  }
+
+  document.querySelector("calcite-shell").hidden = false;
+  document.querySelector("calcite-loader").hidden = true;
+}
